23ccc54ab639d939e3c7d5fb37daa83e930e3a23
Juanfran514 <jeb419@inlumine.ual.es>
2026-04-01 23:57:27 +0200

    feat: Top Teams Scrapper made

--- scripts/teamScrap.js
+++ scripts/teamScrap.js
@@ -3,7 +3,7 @@ const fs = require('node:fs/promises');
 
 // --- CONFIGURACIÓN ---
 const URL_BASE = "https://limitlessvgc.com/teams?time=all&type=regional&format=all&region=all";
-const PAGES_NUM= 10;  //1 page = 25 Teams 
+const PAGES_NUM= 1;  //1 page = 25 Teams 
 const RUTA_SALIDA = 'team_data.json'; 
 
 async function runTeamScrapper(){
@@ -27,7 +27,6 @@ async function runTeamScrapper(){
                 const teamRows = document.querySelectorAll("a.vgc-team");
                 return Array.from(teamRows).map(a => a.href);
             });
-
             allTeamLinks.push(...pageLinks);
 
             if(i<pagesNum){
@@ -43,6 +42,43 @@ async function runTeamScrapper(){
 
         
         console.log(`Total de enlaces capturados: ${allTeamLinks.length}`)
+        console.log(allTeamLinks);
+
+
+        const teamsData = [];
+
+        for (const team of allTeamLinks){
+            console.log("Procesando equipo");
+
+            const teamDetailsPage = await browser.newPage();
+            await teamDetailsPage.setViewport({ width: 1920, height: 1080 });
+
+            await teamDetailsPage.goto(team);
+            
+            const pokemonData = await teamDetailsPage.evaluate(() => {
+                const pokemons = {
+                    name: document.querySelector("body > main > div > div.teamlist > div.teamlist-pokemon > div > div.name > a").innerText.trim(),
+                    item: document.querySelector("body > main > div > div.teamlist > div.teamlist-pokemon > div > div.main > div > div > div.item").innerText.trim(),
+                    ability: document.querySelector("body > main > div > div.teamlist > div.teamlist-pokemon > div > div.main > div > div > div.ability").innerText.trim(),
+                    tera: document.querySelector("body > main > div > div.teamlist > div.teamlist-pokemon > div > div.main > div > div > div.tera").innerText.trim(),
+                    moves: {}
+                    
+                };
+                const pkmMoves = document.querySelector("body > main > div > div.teamlist > div.teamlist-pokemon > div > div.main > div > ul")
+                const movesList = pkmMoves.querySelectorAll("li");
+                movesList.forEach((move, index) => {
+                    pokemons.moves[`move${index+1}`] = move.innerText.trim();
+                });
+
+
+                return pokemons;
+            })
+            teamsData.push(pokemonData);
+
+            await teamDetailsPage.close();
+            console.log("Equipos:");
+            console.log(teamsData);
+        }
 
     } catch(error){
         console.error("Error:",error);
